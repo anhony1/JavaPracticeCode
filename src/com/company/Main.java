04bef08b23b2d6dbbfe98d25d050da24edd9c745
anthonyreyes <areyes5141@gmail.com>
2022-03-22 08:40:23 -0500

    Changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,10 +27,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] intArray = new int[]{3,4,5,6,7,8};
-        //result should be 28
 
-        System.out.println(subsetCheck(intArray));
 
     }
 
